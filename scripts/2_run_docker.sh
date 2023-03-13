20539d599a884e4ea5b8ca3c7a1376a6255e7c34
StephenCha <729steven@gmail.com>
2023-03-13 23:28:41 +0900

    update 23:28:41 03-13-2023

--- scripts/2_run_docker.sh
+++ scripts/2_run_docker.sh
@@ -5,5 +5,8 @@ set -e
 docker run -d -it --gpus=all --shm-size=120G \
     -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix:1 \
     -v $HOME/GitHub/frankmocap:/root/GitHub/frankmocap \
+    -v $HOME/GitHub/IMavatar:/root/GitHub/IMavatar \
+    -v $HOME/GitHub/IMavatar/data:/root/GitHub/IMavatar/data \
+    -v $HOME/GitHub/IMavatar/code/runs:/root/GitHub/IMavatar/code/runs \
     --name frankmocap \
     frankmocap:0.1
