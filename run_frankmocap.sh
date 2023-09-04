2096666608c12dd97ac93cdfce3dbb0e891bb589
HyunsooCha <729steven@gmail.com>
2023-09-04 15:57:02 +0900

    update 15:57:02 09-04-2023

--- run_frankmocap.sh
+++ run_frankmocap.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 set -e
 
-docker exec -e CUDA_VISIBLE_DEVICES=0 frankmocap ./xvfb-run-safe python -m demo.demo_handmocap --input_path $1 --out_dir $2 --view_type ego_centric --save_bbox_output
-docker exec -e CUDA_VISIBLE_DEVICES=0 frankmocap chmod -R 777 $2
+docker exec -e CUDA_VISIBLE_DEVICES=$1 frankmocap ./xvfb-run-safe python -m demo.demo_handmocap --input_path $2 --out_dir $3 --view_type ego_centric --save_bbox_output
+docker exec -e CUDA_VISIBLE_DEVICES=$1 frankmocap chmod -R 777 $3
